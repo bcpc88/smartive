94d4e8190d240b84c129efc3fc07f520ad92d82e
Bartek <bcwiertnia88@gmail.com>
2025-06-28 18:22:23 +0000

    added tests and manual

---
+++ README.md
@@ -0,0 +1,20 @@
+# Instalacja i uruchomienie aplikacji
+
+Konsolowa aplikacja napisana w Symfony, służąca do generowania miniaturek obrazów z lokalnego systemu plików i przesyłania ich na FTP lub zapis lokalny. Aplikacja oparta jest o architekturę heksagonalną i wykorzystuje Symfony Messenger oraz Redis do kolejkowania zadań.
+
+## 🔧 Instalacja
+
+- sklonuj repo i wejdź do katalogu z projektem: **https://github.com/bcpc88/smartive.git**
+- uruchomić dockery: **docker-compose up -d --build**
+- skonfiguruj połączenie FTP do hosta lokalnego lub innego w .env
+- wejdź do dockera php: **docker exec -it smartive-phpfpm bash** i uruchom composer: **composer install**
+
+## 🔧 Uruchomienie
+
+- po instalacji, z poziomu dockera php używamy komendy: **bin/console app:generate-thumbs /ścieżka/do/obrazów /ścieżka/docelowa**
+- domyślnie program działa na lokalnym systemie plików
+- dodatkowe opcje: **--type=ftp** - uruchamia możliwość generowania plików docelowo na skonfigurowany wcześniej serwer FTP
+
+## 👤 Autor
+
+Bartłomiej Ćwiertnia - rekrutacja Smartive.app
---
+++ src/tests/Core/Service/Resolver/ImageWriterResolverTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace BartlomiejCwiertniaRekrutacjaSmartiveapp\Tests\Core\Service\Resolver;
+
+use BartlomiejCwiertniaRekrutacjaSmartiveapp\Adapter\FtpWriter;
+use BartlomiejCwiertniaRekrutacjaSmartiveapp\Adapter\LocalWriter;
+use BartlomiejCwiertniaRekrutacjaSmartiveapp\Core\Service\Resolver\ImageWriterResolver;
+use BartlomiejCwiertniaRekrutacjaSmartiveapp\Port\FilesystemWriterInterface;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class ImageWriterResolverTest extends TestCase
+{
+    public function testResolvesFTPWriterType(): void
+    {
+        $ftpWriter = $this->createMock(FtpWriter::class);
+        $ftpWriter->method('supports')->willReturnCallback(function($type) {
+            return $type === 'ftp';
+        });
+
+        $localWriter = $this->createMock(LocalWriter::class);
+        $localWriter->method('supports')->willReturnCallback(function($type) {
+            return $type === 'local';
+        });
+
+        $resolver = new ImageWriterResolver([$ftpWriter, $localWriter]);
+
+        $this->assertSame($ftpWriter, $resolver->resolve('ftp'));
+    }
+
+    public function testResolvesLocalWriterType(): void
+    {
+        $ftpWriter = $this->createMock(FtpWriter::class);
+        $ftpWriter->method('supports')->willReturnCallback(function($type) {
+            return $type === 'ftp';
+        });
+
+        $localWriter = $this->createMock(LocalWriter::class);
+        $localWriter->method('supports')->willReturnCallback(function($type) {
+            return $type === 'local';
+        });
+
+        $resolver = new ImageWriterResolver([$ftpWriter, $localWriter]);
+
+        $this->assertSame($localWriter, $resolver->resolve('local'));
+    }
+
+    public function testThrowsExceptionWhenNoWriterIsSupported(): void
+    {
+        $writer = $this->createMock(FilesystemWriterInterface::class);
+        $writer->method('supports')->willReturn(false);
+
+        $resolver = new ImageWriterResolver([$writer]);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Unknown writer: unknown');
+
+        $resolver->resolve('unknown');
+    }
+}
